00ec9b4377d0d794f45556e88ddea5311abacc80
Colin Misbach <CMisbach@student.neumont.edu>
2018-03-01 22:38:56 -0700

    Bulk first commit :+1:

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] [Range(0.0f, 25.0f)] float m_spaceFromTarget = 10.0f;
+    [SerializeField] [Range(0.0f, 30.0f)] float m_verticalOffset = 3.0f;
+    [SerializeField] [Range(0.0f, 50.0f)] float m_followDistance = 10.0f;
+    [SerializeField] [Range(1.0f, 20.0f)] float m_lockIntensity = 10.0f;
+    [SerializeField] [Range(1.0f, 20.0f)] float m_attentiveness = 10.0f;
+    [SerializeField] GameObject m_target = null;
+
+    void Start()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+        Vector3 direction = m_target.transform.position - transform.position;
+        if (direction.magnitude >= m_followDistance)
+        {
+            Vector3 dir = transform.position - m_target.transform.position;
+            dir.y = 0.0f;
+            Vector3 offset = dir.normalized * m_spaceFromTarget;
+            Vector3 targetPosition = m_target.transform.position + offset;
+
+            targetPosition.y = m_target.transform.position.y + m_verticalOffset;
+            //if ((targetPosition - m_target.transform.position).y < m_verticalOffset)
+            //    targetPosition += Vector3.up * Time.deltaTime * 7.0f;
+
+            transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * m_attentiveness);
+            //transform.position = targetPosition;
+
+            Quaternion rotation = Quaternion.LookRotation(direction.normalized, Vector3.up);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * m_lockIntensity);
+        }
+        else
+        {
+            Quaternion rotation = Quaternion.LookRotation(direction.normalized, Vector3.up);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * m_lockIntensity);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] [Range(1.0f, 100.0f)] float m_speed = 50.0f;
+    [SerializeField] [Range(1.0f, 20.0f)] float m_turnSpeed = 4.0f;
+    [SerializeField] [Range(1.0f, 10.0f)] float m_jumpResistance = 2.0f;
+    [SerializeField] [Range(1.0f, 10.0f)] float m_fallMultiplier = 2.0f;
+
+    Rigidbody m_rigidbody;
+    float m_speedScale;
+
+    void Start()
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+
+        m_speedScale = m_rigidbody.mass * 50.0f;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            m_rigidbody.AddForce(Vector3.up * 100.0f, ForceMode.Impulse);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 velocity = Vector3.zero;
+        velocity.z = Input.GetAxis("Vertical");
+        velocity.x = Input.GetAxis("Horizontal");
+        velocity = velocity * m_speed * m_speedScale * Time.deltaTime;
+
+        velocity = Camera.main.transform.rotation * velocity; // only make forward affected by camera
+        velocity.y = 0.0f;
+        m_rigidbody.AddForce(velocity, ForceMode.Force);
+
+        if (m_rigidbody.velocity.magnitude > 0.00001f)
+        {
+            Vector3 lookDir = m_rigidbody.velocity.normalized;
+            lookDir.y = 0.0f;
+            Quaternion rotation = Quaternion.LookRotation(lookDir, Vector3.up);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * m_turnSpeed);
+        }
+
+        if (m_rigidbody.velocity.y < 0.0f)
+        {
+            m_rigidbody.velocity += (Vector3.up * Physics.gravity.y) * (m_fallMultiplier - 1.0f) * Time.deltaTime;
+        }
+        else if (m_rigidbody.velocity.y > 0.0f)
+        {
+            m_rigidbody.velocity += (Vector3.up * Physics.gravity.y) * (m_jumpResistance - 1.0f) * Time.deltaTime;
+        }
+    }
+}
